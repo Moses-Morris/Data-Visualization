ee5168fd1ac11cee9d74412e392e68436705c49f
Moses-Morris <mosesmorrisdev@gmail.com>
2023-10-03 18:24:58 +0300

    A LINE CHART.

--- model.py
+++ model.py
@@ -7,4 +7,24 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('overseas-trade-indexes-june-2023-quarter-provisional.csv')
 
 #remove empty fields from file
-no_empty_fields = df.
+no_empty_fields = df.dropna()
+
+print(no_empty_fields.head(8))
+
+
+#print the columns only
+print("Here are the columns:")
+print(df.columns)
+
+x = df["Period"]
+y = df["Data_value"]
+#plot a line chart
+plt.plot(x,y, marker='.', linestyle='-', color='r', label='line label')
+plt.xlabel('Yearly')
+plt.ylabel('Data_value')
+plt.legend()
+plt.title('Overseas Trade Indexes')
+plt.grid(True)
+plt.show()
+
+
